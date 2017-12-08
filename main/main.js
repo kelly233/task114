c4d24603ad2467e1415edc21abf02dbf145bc651
ubutu <1196905733@qq.com>
2017-12-08 11:18:05 +0800

    first release of task114

--- main/main.js
+++ main/main.js
@@ -1,6 +1,18 @@
 // Write your cade below:
-function main() {};
+module.exports = function main(testnumber,mynumber) {
+    let arr1 = testnumber.toString().split('');
+    let arr2 = mynumber.toString().split('');
+    let count1 = 0;
+    let count2 = 0;
+    for(let item of arr1){
+        for(let str of arr2){
+            if(item === str) {count1++;}
+        }
+    }
+    for(let i=0;i<4;i++){
+        if(arr1[i] === arr2[i]){count2++;}
+    }
+    return  count2.toString() +"A" + (count1-count2).toString()+ "B";
 
-module.exports = {
-    main
-}
+};
+
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,107 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('main()', function(){
+   beforeAll(function () {
+       console.log("The testnumber is 1234");
+   })
+    it("0A0B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 5678;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("0A0B");
+        console.log("\n0A0B");
+    })
+    it("0A1B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 5378;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("0A1B");
+        console.log("\n0A1B");
+    })
+    it("0A2B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 5328;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("0A2B");
+        console.log("\n0A2B");
+    })
+    it("0A3B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 4328;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("0A3B");
+        console.log("\n0A3B");
+    })
+
+    it("0A4B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 4321;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("0A4B");
+        console.log("\n0A4B");
+    })
+    it("1A0B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1678;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("1A0B");
+        console.log("\n1A0B");
+    })
+    it("1A1B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1378;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("1A1B");
+        console.log("\n1A1B");
+    })
+    it("1A2B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1328;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("1A2B");
+        console.log("\n1A2B");
+    })
+    it("1A3B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1423;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("1A3B");
+        console.log("\n1A3B");
+    })
+    it("2A0B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1278;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("2A0B");
+        console.log("\n2A0B");
+    })
+    it("2A1B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1248;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("2A1B");
+        console.log("\n2A1B");
+    })
+    it("2A2B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1243;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("2A2B");
+        console.log("\n2A2B");
+    })
+    it("3A0B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1238;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("3A0B");
+        console.log("\n3A0B");
+    })
+    it("4A0B",function () {
+        var Testnumber = 1234;
+        var Mynumber = 1234;
+        var result = main(Testnumber,Mynumber);
+        expect(result).toEqual("4A0B");
+        console.log("\n4A0B");
+    })
 
 });
